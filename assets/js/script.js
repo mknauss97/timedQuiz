371a2ee77d9de3c9c712e3d7553b22c718ad2780
Mknauss <mknauss97@gmail.com>
2021-03-25 17:01:37 -0400

    fixed questions and choices and a little bit of the timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,79 +1,131 @@
 var questions = [
   {
     title: "commonly used data types do not include:",
-    choices:["strings", "booleans", "alerts", "numbers"],
-    answer:"alerts"
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    correctAnswer: 2
   },
   {
     title: "The condition in an If / else statement is enclosed within ___.",
     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "parentheses"
+    correctAnswer: 2
   },
   {
     title: "Arrays in JavaScript can be used to store _____.",
     choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-    answer: "all of the above"
+    correctAnswer: 3
   },
   {
     title: "String values must be enclosed within ____ when being assigned to variables.",
     choices: ["commas", "curly brackets", "quotes", "parentheses"],
-    answer: "quotes"
+    correctAnswer: 2
   },
   {
     title: "A very useful tool used during development and debugging for printing content to the debugger is:",
     choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
-    answer: "console.log"
+    correctAnswer: 3
   }
 ]
-var timerElement = document.querySelector('#timer-count')
+var timerElement = document.querySelector('.timer-count')
 var startButton = document.querySelector('.start-button')
-var questionH = document.querySelector('#question')
-var answerA = document.querySelector('answerA')
-var answerB = document.querySelector('answerB')
-var answerC = document.querySelector('answerC')
-var answerD = document.querySelector('answerD')
-var timerCount;
+var questionH = document.querySelector('.questionCurrent')
+var buttonPlacement = document.querySelector('#butts')
+var highscores = document.querySelector('#scores')
+var timerCount =75
 var scoreCount;
 var currentQuestion = [];
-
-
-
-
+var response = document.querySelector('#response')
+var buttonEl1 = document.querySelector('#button1')
+var buttonEl2 = document.querySelector('#button2')
+var buttonEl3 = document.querySelector('#button3')
+var buttonEl4 = document.querySelector('#button4')
+var buttonArray =[];
+buttonArray.push(buttonEl1)
+buttonArray.push(buttonEl2)
+buttonArray.push(buttonEl3)
+buttonArray.push(buttonEl4)
+console.log(buttonArray)
 var timer = 75;
 function startTimer() {
+  document.getElementById('home-screen').style.display = "none";
+  document.getElementById('quiz-card').style.display = "inline-block"
   // Sets timer
-  timer = setInterval(function() {
-    timerCount --;
+  timer = setInterval(function () {
+    
+    timerCount--;
+    console.log(timerCount)
     timerElement.textContent = timerCount;
-    if (timerCount >= 0) {
-      // Tests if win condition is met
-      if (isWin && timerCount > 0) {
-        // Clears interval and stops timer
-        clearInterval(timer);
-     
-      }
-    }
+    
+    // if (condition) {
+      
+    // }
+    
     // Tests if time has run out
     if (timerCount === 0) {
       // Clears interval
       clearInterval(timer);
-      
+      endGame();
+
     }
   }, 1000);
   askQuestions();
 }
 
-var finalQuestion = questions.length -1;
-var currentQuestion = questions[0]
-var incorrect = ("");
+var finalQuestion = questions.length - 1;
+var currentQuestion = 0
 var correct = ("");
 console.log(currentQuestion)
+console.log(finalQuestion)
 function askQuestions() {
-  for (var i = 0; i <questions.length; i++) {
-    var choices = document.createElement('button')
-    choices.textContent=currentQuestion.choices[i]
-    questionH.append(choices)
+  // var askTitle = document.createElement('h1');
+   questionH.textContent = questions[currentQuestion].title;
+  //  questionH.append(askTitle);
+ 
+  for (var i = 0; i < questions.length; i++) {
+   buttonArray[i].textContent=questions[currentQuestion].choices[i];
+
+
+    // var choices = document.createElement('button');
+    // choices[i].textContent = questions[currentQuestion].choices[i];
+    // correct = questions.correctAnswer
+    // buttonPlacement.appendChild(choices)
   }
 };
+
+function checkAnswer(answer) {
+  if (answer===questions[currentQuestion].correctAnswer) {
+    response.textContent = "Correct"
+    scoreCount += 5;
+  }
+  else {
+    response.textContent = "Incorrect"
+    timerCount-=10;
+  }
+
+  if (currentQuestion < finalQuestion) {
+    currentQuestion++;
+    askQuestions()
+  }
+  else {
+    clearInterval(timer)
+  }
+
+}
+buttonEl1.addEventListener("click",function(){
+  checkAnswer(0)
+})
+buttonEl2.addEventListener("click",function(){
+  checkAnswer(1)
+})
+buttonEl3.addEventListener("click",function(){
+  checkAnswer(2)
+})
+buttonEl4.addEventListener("click",function(){
+  checkAnswer(3)
+})
+console.log(scoreCount)
+function endGame() {
+  document.getElementById('quiz-card').style.display = 'none';
+  document.getElementById('scores').style.display = 'inline-block';
+}
 
 startButton.addEventListener("click", startTimer);
